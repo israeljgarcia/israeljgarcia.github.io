d9dc43adbdfadc950e6614fd5f07c4faea86f151
israeljgarcia <gar19105@byui.edu>
2020-05-02 22:23:29 -0700

    updates link in index and fixes minor footer issue.

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
+let d = new Date();
 document.getElementById("footer-info").innerHTML += " | Last Updated " + document.lastModified.split(" ")[0];
+document.getElementById("year").innerHTML += "" + d.getFullYear();
