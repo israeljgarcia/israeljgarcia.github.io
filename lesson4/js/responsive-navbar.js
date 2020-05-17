a6e4c1e4254374783a0ec1b70326dd5242c109ba
israeljgarcia <gar19105@byui.edu>
2020-05-16 20:49:43 -0700

    Tidies up html layout and adds funtionality for navbar

--- lesson4/js/responsive-navbar.js
+++ lesson4/js/responsive-navbar.js
@@ -1,6 +1,6 @@
-const MENUBUTTON = document.querySelector('.menu-label');
-const NAVIGATIONMENU = document.querySelector('.navigation-menu');
+const MENUBUTTON = document.querySelector('.menu-label');           // Gets the first element with menu-label from DOM
+const NAVIGATIONMENU = document.querySelector('.navigation-menu');  // Gets the first element with navigation-menu from DOM
 
-MENUBUTTON.addEventListener('click', () => 
-    {NAVIGATIONMENU.classList.toggle('responsive')}, false
-);
+MENUBUTTON.addEventListener('click', () =>                          // Adds a listener on MENUBUTTON to check for a click event
+    {NAVIGATIONMENU.classList.toggle('responsive')}, false          // On click triggers callback funtion that adds responsive class 
+);                                                                  // To NAVIGATIONMENU or removes it if it already has it
